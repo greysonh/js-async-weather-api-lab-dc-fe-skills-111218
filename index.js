6eb79d78b711ab91b46fe0995086b0a8a7713b9b
Greyson Harris <gharris@esri.com>
2019-01-24 01:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function fetchCurrentWeather(city) {
 function displayCurrentWeather(json) {
   //render current weather data to the DOM using provided IDs and json from API
   
-  var temp = Math.round(json.main.temp * 9/5 - 459.67);
+  var temp = json.main.temp;
   var low = Math.round(json.main.temp_min * 9/5 - 459.67) + "° F";
   var high = Math.round(json.main.temp_max * 9/5 - 459.67) + "° F";
   var humidity = json.main.humidity + "%";
